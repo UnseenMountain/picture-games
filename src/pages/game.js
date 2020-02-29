7f4b6bb121b17284392eaea0d4f9fc66beec44c3
Luke Klymshyn <unseenmountain@gmail.com>
2020-02-28 17:47:51 -0800

    switched files around

---
+++ src/pages/game.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Jumbotron, Card} from "react-bootstrap";
+
+function Game() {
+
+    return(
+        <div>
+            <Jumbotron>
+                <h1>Welcome to picture clicker</h1>
+            </Jumbotron>
+
+            
+        </div>
+    )
+}
