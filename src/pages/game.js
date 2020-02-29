6366d7eecef6d8f29964800c6ad2174d45bd808f
Luke Klymshyn <unseenmountain@gmail.com>
2020-02-28 18:39:36 -0800

    added two new npm packages

--- src/pages/game.js
+++ src/pages/game.js
@@ -9,7 +9,10 @@ function Game() {
                 <h1>Welcome to picture clicker</h1>
             </Jumbotron>
 
-            
+            <Card>
+                <p>Game Box</p>
+            </Card>
         </div>
     )
 }
+export default Game;
