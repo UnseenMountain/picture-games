0001a42846f274e71a5ccd075fc8c4ffec06409a
Luke Klymshyn <unseenmountain@gmail.com>
2020-03-27 09:03:04 -0700

    making heroku app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 // import "tachyons";
 
 const style = {
-  bgDark: {
+  bgLight: {
     backgroundColor: `rgb(1,0,1)`
 
 
@@ -10,7 +10,7 @@ const style = {
 }
 
 const NavBar = props => (
-  <header style={style.bgDark} className=" w-100 ph3 pv3 pv2-ns ph4-m ph5-l tc">
+  <header style={style.bgLight} className=" w-100 ph3 pv3 pv2-ns ph4-m ph5-l tc">
     <nav className="f6 fw6 ttu tracked">
       <p className="link  white dib mr3"  title="Home">The Game</p>
       <p className="link  white dib mr3" >Status: {props.status}</p>
